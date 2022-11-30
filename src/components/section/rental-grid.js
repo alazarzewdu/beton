85f70fbda9dc081d1a4d990db25db1892505b479
Alazar Zewdu <alazarzewdu3@gmail.com>
2022-11-30 13:21:26 +0300

    added more button replacing the original pagination

--- src/components/section/rental-grid.js
+++ src/components/section/rental-grid.js
@@ -52,6 +52,153 @@ class PropertyGrid extends Component {
 			          </div>         
 			        </div>
 			      </div>
+
+				  <div className="col-lg-4 col-md-6">
+			        <div className="single-product-wrap style-2">
+			          <div className="thumb">
+			            <img src={publicUrl+"assets/img/project/15.png" }alt="img" />
+			            <div className="product-wrap-details">
+			              <div className="media">
+			                <div className="author">
+			                    
+			                </div>
+			                <div className="media-body">
+			                  <p><img src={publicUrl+"assets/img/icon/location-alt.png"} alt="img" />Apartment</p>
+			                </div>
+			              </div>
+			            </div> 
+			          </div> 
+			          <div className="product-details-inner">
+			            <h4><Link to="/property-details"> 32,000,000$</Link></h4>
+			            <ul className="meta-inner">
+			            </ul>
+			            <p>Bole, Wolo Sefer</p>
+			            <span className="">Addis Ababa</span>
+			          </div>
+			          <div className="product-meta-bottom style-2">
+			            <span>3 <span>Bedroom</span></span>
+			            <span className="border-none">2 <span>Bathroom</span></span>
+			            <span>1026 <span>sqft</span></span>
+						
+			          </div>         
+			        </div>
+			      </div><div className="col-lg-4 col-md-6">
+			        <div className="single-product-wrap style-2">
+			          <div className="thumb">
+			            <img src={publicUrl+"assets/img/project/15.png" }alt="img" />
+			            <div className="product-wrap-details">
+			              <div className="media">
+			                <div className="author">
+			                    
+			                </div>
+			                <div className="media-body">
+			                  <p><img src={publicUrl+"assets/img/icon/location-alt.png"} alt="img" />Apartment</p>
+			                </div>
+			              </div>
+			            </div> 
+			          </div> 
+			          <div className="product-details-inner">
+			            <h4><Link to="/property-details"> 32,000,000$</Link></h4>
+			            <ul className="meta-inner">
+			            </ul>
+			            <p>Bole, Wolo Sefer</p>
+			            <span className="">Addis Ababa</span>
+			          </div>
+			          <div className="product-meta-bottom style-2">
+			            <span>3 <span>Bedroom</span></span>
+			            <span className="border-none">2 <span>Bathroom</span></span>
+			            <span>1026 <span>sqft</span></span>
+						
+			          </div>         
+			        </div>
+			      </div><div className="col-lg-4 col-md-6">
+			        <div className="single-product-wrap style-2">
+			          <div className="thumb">
+			            <img src={publicUrl+"assets/img/project/15.png" }alt="img" />
+			            <div className="product-wrap-details">
+			              <div className="media">
+			                <div className="author">
+			                    
+			                </div>
+			                <div className="media-body">
+			                  <p><img src={publicUrl+"assets/img/icon/location-alt.png"} alt="img" />Apartment</p>
+			                </div>
+			              </div>
+			            </div> 
+			          </div> 
+			          <div className="product-details-inner">
+			            <h4><Link to="/property-details"> 32,000,000$</Link></h4>
+			            <ul className="meta-inner">
+			            </ul>
+			            <p>Bole, Wolo Sefer</p>
+			            <span className="">Addis Ababa</span>
+			          </div>
+			          <div className="product-meta-bottom style-2">
+			            <span>3 <span>Bedroom</span></span>
+			            <span className="border-none">2 <span>Bathroom</span></span>
+			            <span>1026 <span>sqft</span></span>
+						
+			          </div>         
+			        </div>
+			      </div><div className="col-lg-4 col-md-6">
+			        <div className="single-product-wrap style-2">
+			          <div className="thumb">
+			            <img src={publicUrl+"assets/img/project/15.png" }alt="img" />
+			            <div className="product-wrap-details">
+			              <div className="media">
+			                <div className="author">
+			                    
+			                </div>
+			                <div className="media-body">
+			                  <p><img src={publicUrl+"assets/img/icon/location-alt.png"} alt="img" />Apartment</p>
+			                </div>
+			              </div>
+			            </div> 
+			          </div> 
+			          <div className="product-details-inner">
+			            <h4><Link to="/property-details"> 32,000,000$</Link></h4>
+			            <ul className="meta-inner">
+			            </ul>
+			            <p>Bole, Wolo Sefer</p>
+			            <span className="">Addis Ababa</span>
+			          </div>
+			          <div className="product-meta-bottom style-2">
+			            <span>3 <span>Bedroom</span></span>
+			            <span className="border-none">2 <span>Bathroom</span></span>
+			            <span>1026 <span>sqft</span></span>
+						
+			          </div>         
+			        </div>
+			      </div><div className="col-lg-4 col-md-6">
+			        <div className="single-product-wrap style-2">
+			          <div className="thumb">
+			            <img src={publicUrl+"assets/img/project/15.png" }alt="img" />
+			            <div className="product-wrap-details">
+			              <div className="media">
+			                <div className="author">
+			                    
+			                </div>
+			                <div className="media-body">
+			                  <p><img src={publicUrl+"assets/img/icon/location-alt.png"} alt="img" />Apartment</p>
+			                </div>
+			              </div>
+			            </div> 
+			          </div> 
+			          <div className="product-details-inner">
+			            <h4><Link to="/property-details"> 32,000,000$</Link></h4>
+			            <ul className="meta-inner">
+			            </ul>
+			            <p>Bole, Wolo Sefer</p>
+			            <span className="">Addis Ababa</span>
+			          </div>
+			          <div className="product-meta-bottom style-2">
+			            <span>3 <span>Bedroom</span></span>
+			            <span className="border-none">2 <span>Bathroom</span></span>
+			            <span>1026 <span>sqft</span></span>
+						
+			          </div>         
+			        </div>
+			      </div>
 			      
 			      
 			      
@@ -60,8 +207,10 @@ class PropertyGrid extends Component {
 			      
 			      
 			      
-			     
-				 
+				  <div className="col-md-3">
+			                <Link className="btn btn-base2 w-90" to=""> More</Link>
+			              </div>
+
 			    </div>
 			  </div>
 			</div>
