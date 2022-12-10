f64b1d25e9e2d5dcf2ccd2a5cac19df6e62c4c98
Alazar Zewdu <alazarzewdu3@gmail.com>
2022-12-10 16:46:46 +0300

    added tips section

--- src/components/section/agent.js
+++ src/components/section/agent.js
@@ -15,6 +15,8 @@ class Agent extends Component {
               <h2>Tips</h2>
               <h6>Hand picked tips from the team Beton</h6>
             </div>
+
+         {/*  <div className='test'>
             <div className="row justify-content-center">
               <div className="col-lg-4 col-md-6">
                 <div className="single-agent-wrap">
@@ -28,41 +30,29 @@ class Agent extends Component {
                   </div>        
                 </div>
               </div>
+              </div> */}
 
-              <div className="col-lg-4 col-md-6">
-                <div className="single-agent-wrap">
-                  <div className="thumb">
-                    <img src={publicUrl+"assets/img/agent/1.png"} alt="img" />
-                  </div> 
-                  <div className="details">
-                    <h4>Head Title</h4>
-                    <p>short info short info short info short info  short info short info short info short info  short info... </p>
-                    <Link to="/tips" className='read_more'>Read More</Link>
-                  </div>        
+             
+                <div className='tips-t'>
+                  <div className='tips-img'>
+                  <img src={publicUrl+"assets/img/project/tips.webp" }alt="img" />
+                    <h4 className='tips-title'>Tips</h4>
+                  </div>
+                  <div className='tips-det'>
+                    <p>All things you need to know about buying and renting houses from team Beton</p>
+                    <Link className="btn btn-baset w-10" to="/tips">Read More</Link>
+                    </div>
                 </div>
-              </div>
-
-              <div className="col-lg-4 col-md-6">
-                <div className="single-agent-wrap">
-                  <div className="thumb">
-                    <img src={publicUrl+"assets/img/agent/1.png"} alt="img" />
-                  </div> 
-                  <div className="details">
-                    <h4>Head Title</h4>
-                    <p>short info short info short info short info  short info short info short info short info  short info... </p>
-                    <Link to="/tips" className='read_more'>Read More</Link>
-                  </div>        
-                </div>
-              </div>
+              
               
 
-              
+            
               
                
               
             </div>
           </div>
-        </div>
+    
 
 
         }
